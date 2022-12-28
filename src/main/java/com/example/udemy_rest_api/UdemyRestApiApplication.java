a5e86b076aad74a43d28d5538195280eac786406
Gavin-1001 <gavinshelley10@gmail.com>
2022-12-28 21:53:47 +0000

    Init commit for project

---
+++ src/main/java/com/example/udemy_rest_api/UdemyRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyRestApiApplication.class, args);
+    }
+
+}
