59ca60bda8269ae7931753f09742dfb9ef2e77cc
Gavin Shelley <gavinshelley10@gmail.com>
2023-01-10 15:21:29 +0000

    FINISH video 50

--- src/main/java/com/example/udemy_rest_api/repository/StudentRepository.java
+++ src/main/java/com/example/udemy_rest_api/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.example.udemy_rest_api.repository;
 import com.example.udemy_rest_api.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -27,4 +28,8 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByFirstNameLike(String firstName);
 
+
+    //Using JPQL
+    @Query("from student_table where first_name = "Gavin" and last_name= \"Shelley\"")
+    Student getByFirstNameOrLastName(String lastName, String firstName);
 }
